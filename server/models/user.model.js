cad0812a0229a11226694af7e01baf232ab4440b
Gaurav Sharma <gs236934@gmail.com>
2024-11-21 19:53:05 +0530

    integrate jwt and bcrypt

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -9,5 +11,36 @@ const userSchema = new mongoose.Schema({
   isOnline: {type : String , default : "0"},
   createdAt: { type: Date, default: Date.now },
 });
+
+userSchema.pre("save" , async function (next){
+  if(!this.isModified("password")) return next()
+  this.password = await bcrypt.hash(this.password , 10);
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+  jwt.sign({
+    _id : this._id,
+    email : this.email,
+    name : this.name
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+  })
+}
+userSchema.methods.generateRefreshToken = function (){
+  jwt.sign({
+    _id : this._id
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+  })
+}
 
 export default mongoose.model('User', userSchema);
